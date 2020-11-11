a05a6b8bd66c352bb97325273db8e7daf5cfb31f
alfredopl <aalfredoplz@gmail.com>
2020-11-11 14:24:23 -0300

    Completed part 2

--- operation.sql
+++ operation.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE post;
 
-CREATE TABLE post (id SMALLINT, username VARCHAR(25), 
+CREATE TABLE post (id INT PRIMARY KEY, username VARCHAR(25), 
 date_creation DATE, contenido VARCHAR(200), descriptions VARCHAR(200));
 
 INSERT INTO post (id, username, date_creation, contenido, descriptions)
@@ -30,3 +30,39 @@ DELETE FROM post WHERE id = 2;
 INSERT INTO post (id, username, date_creation, contenido, descriptions,titulo)
 VALUES (6,'Carlos','2020-10-10', 'Nuevo contenido','Esta es la descripcion del nuevo contenido de carlos','titulo del post de carlos new');
 
+CREATE TABLE comentarios (id INT , date_h TIMESTAMP, content VARCHAR(200), FOREIGN KEY (id) REFERENCES post(id));
+
+
+INSERT INTO comentarios (id, date_h, content)
+VALUES (1,'2020-10-11 12:10:25', 'algo del comentario');
+
+
+INSERT INTO comentarios (id, date_h, content)
+VALUES (1,'2020-10-11 12:12:35', 'respuesta del comentario');
+
+
+INSERT INTO comentarios (id, date_h, content)
+VALUES (6,'2020-10-11 12:13:35', 'comentario 1');
+
+INSERT INTO comentarios (id, date_h, content)
+VALUES (6,'2020-10-11 12:14:35', 'comentario 2');
+
+INSERT INTO comentarios (id, date_h, content)
+VALUES (6,'2020-10-11 12:15:35', 'comentario 3');
+
+INSERT INTO comentarios (id, date_h, content)
+VALUES (6,'2020-10-11 12:16:35', 'comentario 4');
+
+INSERT INTO post (id, username, date_creation, contenido, descriptions,titulo)
+VALUES (7,'Margarita','2020-10-11', 'Contenido de margarita','Esta es la descripcion del primer post de Pedro','titulo del post de margarita');
+
+INSERT INTO comentarios (id, date_h, content)
+VALUES (7,'2020-10-11 15:30:35', 'comentario para margarita 1');
+INSERT INTO comentarios (id, date_h, content)
+VALUES (7,'2020-10-11 15:31:35', 'comentario para margarita 2');
+INSERT INTO comentarios (id, date_h, content)
+VALUES (7,'2020-10-11 15:32:35', 'comentario para margarita 3');
+INSERT INTO comentarios (id, date_h, content)
+VALUES (7,'2020-10-11 15:33:35', 'comentario para margarita 4');
+INSERT INTO comentarios (id, date_h, content)
+VALUES (7,'2020-10-11 15:34:35', 'comentario para margarita 5');
